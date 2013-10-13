d93d4879f580f6cf6e4779725f6544833dc4c4bd
Andrey Kutyrev <andrey.kutyrev@gmail.com>
2013-10-13 15:18:15 +0600

    Fix settings.

--- project/settings/production.py
+++ project/settings/production.py
@@ -1,3 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from common import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "project.db",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    }
+}
